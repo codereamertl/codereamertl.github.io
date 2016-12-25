366de24dde38a9be30d61b24e2da989d75817f6b
Ihor Orlovskyi <ihororlovskyi@gmail.com>
2016-12-25 17:38:15 +0200

    upd gulpfile.js

---
+++ gulpfile.js/tasks/js-concat.js
@@ -0,0 +1,11 @@
+var gulp = require('gulp'),
+    concat = require('gulp-concat');
+
+gulp.task('js-concat', ['sass'], function () {
+    return gulp.src([
+            '_js/lib/*.js',
+            '_js/common/*.js'
+        ])
+        .pipe(concat('bundle.js'))
+        .pipe(gulp.dest('assets/js/'));
+});
